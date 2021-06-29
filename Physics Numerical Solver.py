914a1baef31d05f880d43ab801a363bb892b142c
Samarth-Bansal <samarthbansal0123@gmail.com>
2021-06-29 20:59:39 +0530

    Changes

--- Physics Numerical Solver.py
+++ Physics Numerical Solver.py
@@ -1,17 +1,25 @@
-print('*______________________________THE BASIC PHYSICS NUMERICAL SOLVER______________________________*')
+import math
+
+print('*-------------------------------THE BASIC PHYSICS NUMERICAL SOLVER--------------------------------*')
 print("Chapter 1. Electric Charges and Fields")
 print("Chapter 2. Electrostatic Potential And Capacitance")
 print("Chapter 3. Current Electricity")
-print('++++++++++++++++++++++++++++++++++++++++++++++++++++')
-print('For:')
+print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+print('For:-')
 print('Volt=V, Current=I, Resistance=R, Charge=q, Radius=r, Electric Field=E, Power=P, Capacitance=C, Force=F')
 print('Area=A, Distance=d, Drift velocity=Vd, Dielectric constant=K, Work done=W, Electric flux=Ef, Length=L')
 print("Charge density=p, Induced charge=q', Polarisation=P', Volume=V, Two Charges=q1q2, Two Radius=r1r2")
+print('Electromotive force=emf, Stored energy=u, No. of Electron=n ,Potential difference=U ')
+print('*Shape=shape, Angle=x')
 print('#Note: All value must be in Standard Unit')
+print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
 data=str(input("Which Values do Question have: "))
 lst= data.split(',')
 print(lst)
 want=str(input("What Do Question want: "))
+k=9*(10**9)
+e0=8.85*(10**-12)
+e=1.6*10**-19
 if want=='V':
   V=[['I','R'],['P','I'],['P','R'],['q','C'],['P','E','q'],['q','r']]
   for i in range(0,len(V)):
@@ -25,36 +33,98 @@ if want=='V':
           I=int(input('I='))
           R=int(input('R='))
           Volt=I*R
-          print('Voltage is ',Volt,'V')
+          print('Voltage(I*R) is ',Volt,'V')
         if Vnew ==[['I','P']]:
           I=int(input('I='))
           P=int(input('P='))
           Volt=P/I
-          print('Voltage is ',Volt,'V')
+          print('Voltage(P/I) is ',Volt,'V')
         if Vnew ==[['P','R']]:
           R=int(input('R='))
           P=int(input('P='))
           Volt=P*R
-          print('Voltage is ',Volt,'V')
+          print('Voltage(P*R) is ',Volt,'V')
         if Vnew ==[['I','R']]:
           I=int(input('I='))
           R=int(input('R='))
           Volt=I*R
-          print('Voltage is ',Volt,'V')
+          print('Voltage(I*R) is ',Volt,'V')
         if Vnew ==[['P','E','q']]:
           P=int(input('P='))
           E=int(input('E='))
           q=int(input('q='))
           Volt=P*E/q
-          print('Voltage is ',Volt,'V')
+          print('Voltage is(P*E/q) ',Volt,'V')
         if Vnew ==[['q','r']]:
           q=int(input('q='))
           r=int(input('r='))
-          Volt=(9*10**9)*q/r
-          print('Voltage is ',Volt,'V')
-          
-          
-                
+          Volt=(k)*q/r
+          print('Voltage is(k*q/r) ',Volt,'V')
+      else:
+          if lst != V[i]:
+            print('We cannot find Voltage from Given Information')
+          break
 
-  
+elif want=='C':
+  C=[['q','V'],['A','d'],['U','V'],['U','q'],['n','V'],['r1r2'],['r1r2','L']]
+  for i in range(0,len(C)):
+      lst.sort()
+      C[i].sort()
+      if lst == C[i]:
+        print ("We can find Capacitance from Given Information")
+        Cnew=[]
+        Cnew.append(C[i])
+        if Cnew ==[['q','V']]:
+          q=int(input('q='))
+          V=int(input('V='))
+          Cap=q/V
+          print('Capacitance is() ',Cap,'F')
+        if Cnew ==[['U','V']]:
+          U=int(input('U='))
+          V=int(input('V='))
+          Cap=2*U/(V**2)
+          print('Capacitance is() ',Cap,'F')
+        if Cnew ==[['q','U']]:
+          q=int(input('q='))
+          U=int(input('U='))
+          Cap=q**2/2*U
+          print('Capacitance is() ',Cap,'F')
+        if Cnew ==[['n','V']]:
+          n=int(input('n='))
+          V=int(input('V='))
+          Cap=n*e/V
+          print('Capacitance is() ',Cap,'F')
+        if Cnew ==[['A','d']]:
+          A=int(input('A='))
+          d=int(input('d='))
+          Cap=e0*A/d
+          print('Capacitance is() ',Cap,'F')
+        if Cnew ==[['q','r1r2']]:
+          q=int(input('q='))
+          r1=int(input('r1(inner)='))
+          r2=int(input('r2(outer)='))
+          Cap=(4*(3.14)*e0*r1*r2)/r1 - r2
+          print('Capacitance is() ',Cap,'F')
+        if Cnew ==[['L','r1r2']]:
+          L=int(input('L='))
+          r1=int(input('r1(inner)='))
+          r2=int(input('r2(outer)='))
+          Cap=(2*(3.14)*e0*L)/(math.log(r2/r1))
+          print('Capacitance is() ',Cap,'F')
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
   
