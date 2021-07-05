6138e65eff08f1d13ac926721d85c6cbc0de465f
Virentanti <Virentanti16@gmail.com>
2021-07-05 18:51:07 +0530

    now importing questions in a text file

--- scrapy_thingy.py
+++ scrapy_thingy.py
@@ -5,13 +5,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 driver = webdriver.Firefox(r"C:\Users\Rashmi\Desktop\Development\one-good-project")
 
 
-text=''
-
+text=[]
+file=open("yay.txt",'a')
 for i in range(84237,84259):
     url='https://www.studyadda.com/question-bank/assertion-and-reason_q2/1162/84237'
     url.replace(str(84327),str(i))
     driver.get(url)
-    text+=driver.find_element_by_xpath("/html/body/div[2]/div/div/section/div[1]/div[1]/div").text
-
-#driver.implicitly_wait(10)
+    txt=driver.find_element_by_xpath("/html/body/div[2]/div/div/section/div[1]/div[1]/div").text
+    file.write(txt)
+    for i in range(5):
+        file.write("")
+    text.append(txt)
 print(text)
