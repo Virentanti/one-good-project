d018ea22e1bc3453603214e86da8e5fcf074e27f
Virentanti <Virentanti16@gmail.com>
2021-07-05 20:11:35 +0530

    bleh bleh bleh

--- scrapy_thingy.py
+++ scrapy_thingy.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Firefox(r"C:\Users\Rashmi\Desktop\Development\one-good-project")
+def scrape(url,x,stp):
 
+    driver = webdriver.Firefox(r"C:\Users\Rashmi\Desktop\Development\one-good-project")
+    rep=url[-5:]
+    url=url[:-5]
 
-text=[]
-file=open("yay.txt",'a')
-for i in range(84237,84259):
-    url='https://www.studyadda.com/question-bank/assertion-and-reason_q2/1162/84237'
-    url.replace(str(84327),str(i))
-    driver.get(url)
-    txt=driver.find_element_by_xpath("/html/body/div[2]/div/div/section/div[1]/div[1]/div").text
-    file.write(txt)
-    for i in range(5):
-        file.write("")
-    text.append(txt)
-print(text)
+    text=[]
+    file=open(x,'a')
+    for i in range(rep,stp):
+        d_url=url+str(i)
+        driver.get(d_url)
+        txt=driver.find_element_by_xpath("/html/body/div[2]/div/div/section/div[1]/div[1]/div").text
+        file.write(txt)
+        for i in range(5):
+            file.write("\n")
+        text.append(txt)
