72c7c05c9e2831ea6da2e075d8ccf1e3a14afe26
Virentanti <Virentanti16@gmail.com>
2021-07-18 16:50:20 +0530

    almost done

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
-import os
-from scrapy_thingy import scrape
+import sys
+import platform
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
 
-chapter_lst=['Physics Electrostatics & Capacitance','Current Electricity, Charging & Discharging Of Capacitors']
-#types=[['Assertion and Reason', 'Critical Thinking','Grouping of Capacitors','Capacitance','Electric Flux and Gausss Law','Electric Dipole','Electric Field and Potential','Charge and Coulombs Law','Electric Charges and Field Conceptual Problems'],['Assertion and Reason', 'Critical Thinking','Thermo Electricity','Chemical Effect of Current','Heating Effect of Current','Assertion and Reason', 'Critical Thinking','Different Measuring Instruments',"Kirchhoff's Law, Cells",'Grouping of Resistances',"Electric Conduction, Ohm's and Resistance",'Different Measuring Instruments','Kirchhoffs Law Cells','Grouping of Resistances','Electric Conduction Ohms and Resistance']]
+# GUI FILE
+from ui_main import Ui_MainWindow
 
-dat={'Assertion_and_Reason':['https://www.studyadda.com/question-bank/assertion-and-reason_q1/1162/84237',21],'Critical Thinking':['https://www.studyadda.com/question-bank/critical-thinking_q1/1160/84183',45],'Thermo Electricity':['https://www.studyadda.com/question-bank/thermo-electricity_q1/1159/84139',44],'Chemical Effect of Current':['https://www.studyadda.com/question-bank/chemical-effect-of-current_q1/1158/84080',59],'Heating Effect of Current':['https://www.studyadda.com/question-bank/heating-effect-of-current_q1/1157/83927',153],'Assertion and Reason_2':['https://www.studyadda.com/question-bank/assertion-and-reason_q1/1068/77282',17],'Critical Thinking_2':['https://www.studyadda.com/question-bank/critical-thinking_q1/1066/77208',54],'Different Measuring Instruments':['https://www.studyadda.com/question-bank/different-measuring-instruments_q1/1065/77065',143],'Kirchhoff Law, Cells':['https://www.studyadda.com/question-bank/kirchhoffs-law-cells_q1/1064/76980',85],'Grouping of Resistances':['https://www.studyadda.com/question-bank/grouping-of-resistances_q1/1063/76839',141],'Electric Conduction & Ohm and Resistance':['https://www.studyadda.com/question-bank/electric-conduction-ohms-and-resistance_q1/1062/76706',133],'Different Measuring Instruments_2':['https://www.studyadda.com/question-bank/different-measuring-instruments_q1/1051/75677',143],'Kirchhoffs Law Cells':['https://www.studyadda.com/question-bank/kirchhoffs-law-cells_q1/1050/75592',85],'Grouping of Resistances':['https://www.studyadda.com/question-bank/grouping-of-resistances_q1/1049/75451',141],'Electric Conduction Ohms and Resistance':['https://www.studyadda.com/question-bank/electric-conduction-ohms-and-resistance_q1/1048/75318',133]}
-type=list(dat.keys())
-directory = chapter_lst[1]
-parent_dir = r"C:\Users\Rashmi\Desktop\Development\one-good-project\content"
-path = os.path.join(parent_dir, directory)
-#os.mkdir(path)
-for j in type:
-    x=os.path.join(r"C:\Users\Rashmi\Desktop\Development\one-good-project\content\Current Electricity", j)
-    scrape(dat[j][0],x,dat[j][1])
+# IMPORT FUNCTIONS
+from ui_functions import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        UIFunctions.frameless(self)
+
+
+        def move_window(event):
+            if event.buttons() == Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.dragPos)
+                self.dragPos = event.globalPos()
+                event.accept()
+
+        self.ui.titlebar_frame.mouseMoveEvent = move_window
+
+        ## TOGGLE/BURGUER MENU
+        ########################################################################
+        self.ui.menu_btn.clicked.connect(lambda: UIFunctions.toggleMenu(self, 170, True))
+
+        # PAGE 1
+        self.ui.home_btn.clicked.connect(lambda: self.ui.stacked_widgets.setCurrentWidget(self.ui.home))
+
+        # PAGE 2
+        self.ui.generate_btn.clicked.connect(lambda: self.ui.stacked_widgets.setCurrentWidget(self.ui.generate))
+
+        self.ui.browse_btn.clicked.connect(lambda: UIFunctions.browse(self))
+
+
+
+        ## SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+        ## ==> END ##
+
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
--- ui_functions.py
+++ ui_functions.py
@@ -1,7 +1,10 @@
 ## ==> GUI FILE
 from main import *
 
+GLOBAL_STATE=0
+
 class UIFunctions(MainWindow):
+    
 
     def toggleMenu(self, maxWidth, enable):
         if enable:
@@ -31,3 +34,33 @@ class UIFunctions(MainWindow):
             self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
 
             self.animation.start()
+    def maximize_restore(self):
+        global GLOBAL_STATE
+        glo=GLOBAL_STATE
+        if glo==0:
+            self.showMaximized()
+            GLOBAL_STATE=1
+
+            #self.ui.central_frame.setContentsMargins(0,0,0,0)
+            self.ui.restore_btn.setToolTip("Restore")
+        else:
+            GLOBAL_STATE=0
+            self.showNormal()
+
+            self.resize(self.width()+1, self.height()+1)
+            self.ui.restore_btn.setToolTip("Maximize")
+
+    def frameless(self):
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.ui.restore_btn.clicked.connect(lambda: UIFunctions.maximize_restore(self))
+        self.ui.minimize_btn.clicked.connect(lambda: self.showMinimized())
+        self.ui.close_btn.clicked.connect(lambda: self.close())
+
+
+    def browse(self):
+        fname=QFileDialog.getExistingDirectory(self, 'Select Directory')
+        print(fname)
+        #self.fname.setText(fname[0])
+
